655f28ff5daec25469e8a6ae23f125fcec062fad
Dina Lorenz <dinilor90@gmail.com>
2017-01-05 21:02:12 +0200

    first commit

---
+++ Schedule/app/app.route.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+    angular.module('schApp')
+    .config(['$routeProvider', function config($routeProvider) {
+        $routeProvider
+          .when('/login', {
+                controller: 'loginCtrl',
+                templateUrl: 'login/login.tpl.html',
+                controllerAs: 'loginVm'
+            })
+               .when('/sign', {
+                   controller: 'signCtrl',
+                   controllerAs:'signVm',
+                   templateUrl: 'sign/sign.tpl.html'
+               })
+            .when('/report', {
+                controller: 'reportCtrl',
+                templateUrl: 'report/report.tpl.html',
+                controllerAs: 'reportVm'
+            })
+           
+            .otherwise({
+                redirectTo: '/login'
+            });
+    }]);
+    
+})();
